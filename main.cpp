c3b0a9863c008f2e8f66a0e5f72dae1ccf25ad47
jinly <jinlee0703@naver.com>
2023-12-04 17:24:06 +0900

    231204

--- main.cpp
+++ main.cpp
@@ -4,25 +4,31 @@
 #pragma comment(lib, "SDL2")
 #pragma comment(lib, "SDL2main")
 
+// OS main() -> SDL_main
 
 int SDL_main(int argc, char* argv[])
 {
 	srand((unsigned int)time(NULL));
 
-	SDL_Init(SDL_INIT_EVERYTHING);//초기화
+	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_Window* MyWindow = SDL_CreateWindow("HelloWorld", 100, 100, 800, 600,
 		SDL_WINDOW_OPENGL);
 	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, -1, SDL_RENDERER_ACCELERATED |
 		SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
 
+	Uint64 DeltaSeconds = 0();
+	Uint64 LastTime = 0();
 	SDL_Event MyEvent;
 	bool IsRunning = true;
 	while (IsRunning)
 	{
+		DeltaSeconds = SDL_GetTicks64() - LastTime;
+		LastTime = SDL_GetTicks64();
 		SDL_PollEvent(&MyEvent);
 		switch (MyEvent.type)
 		{
+
 		case SDL_QUIT:
 			IsRunning = false;
 			break;
@@ -36,7 +42,7 @@ int SDL_main(int argc, char* argv[])
 		SDL_SetRenderDrawColor(MyRenderer, 0x0b, 0x0b, 0x61, 0x00);
 		SDL_RenderClear(MyRenderer);
 
-		for (int i = 0; i < 1000; ++i)
+		for (int i = 0; i < 100; ++i)
 		{
 			int X = rand() % 800;
 			int Y = rand() % 600;
@@ -48,6 +54,7 @@ int SDL_main(int argc, char* argv[])
 		}
 		SDL_RenderPresent(MyRenderer); // 개시!
 		
+	
 	}
 	SDL_Quit();//종료
 	return 0;
